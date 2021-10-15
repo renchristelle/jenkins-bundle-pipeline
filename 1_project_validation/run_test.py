c1709fe2023f2d20ebd2dad21cd1f000c0163ee6
mgalloway <matthew.galloway19@gmail.com>
2021-10-15 17:30:07 +0100

    modifying minimum limit for code complexity

--- 1_project_validation/run_test.py
+++ 1_project_validation/run_test.py
@@ -31,7 +31,7 @@ def test_coding_recipes_complexity(params):
             payload = project.get_recipe(recipe["name"]).get_settings().get_code()
             code_analysis = cc_raw.analyze(payload)
             print(code_analysis)
-            assert code_analysis.loc < 100
+            assert code_analysis.loc < 2000
             assert code_analysis.lloc < 50
             v = cc_visitors.ComplexityVisitor.from_code(payload)
             assert v.complexity < 21, "Code complexity of recipe " + recipe["name"] + " is too complex: " + v.complexity + " > max value (21)"
