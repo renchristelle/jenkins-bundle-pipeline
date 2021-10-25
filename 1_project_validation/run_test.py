eb3b81f19830409da7eb4ccc98b1f3d1c656bb3b
Christelle Ren <58270436+renchristelle@users.noreply.github.com>
2021-10-25 18:07:57 +0200

    Update run_test.py

--- 1_project_validation/run_test.py
+++ 1_project_validation/run_test.py
@@ -1,7 +1,6 @@
 import dataikuapi
 import radon.raw as cc_raw
 import radon.visitors as cc_visitors
-from helpers import trigger_scenario
 
 
 def test_scenario(params):
@@ -12,29 +11,15 @@ def test_scenario(params):
     scenarios = project.list_scenarios()
     test_scenario = False
     smoketest_scenario = False
-
-    smoke_scenario_run = trigger_scenario(scenario["TEST_SMOKE"])
-    unit_scenario_run = trigger_scenario(scenario["TEST_UNIT"])  # actual details of run after checking for trigger
-
-    while True:
-        smoke_scenario_run.refresh()
-        unit_scenario_run.refresh()
-
-        if not (smoke_scenario_run.running and unit_scenario_run):
-            if smoke_scenario_run.outcome == "SUCCESS":
+    for scenario in scenarios:
+        if scenario["id"].startswith("TEST"):
+            test_scenario = True
+            if scenario["id"] == "TEST_SMOKE":
                 smoketest_scenario = True
-            else:
-                print(f"Smoke Test Error was: {moke_scenario_run.get_details().first_error_details}")
-            if smoke_scenario_run.outcome == "SUCCESS":
-                unit_scenario_run = True
-            else:
-                print(f"Unit Test Error was: {smoke_scenario_run.get_details().first_error_details}")
-            break
-
-    assert unit_scenario_run, "You need at least one test scenario (name starts with 'TEST_')"
+    assert test_scenario, "You need at least one test scenario (name starts with 'TEST_')"
     assert smoketest_scenario, "You need at least one smoke test scenario (name 'TEST_SMOKE')"
 
-
+    
 def test_coding_recipes_complexity(params):
     client = dataikuapi.DSSClient(params["host"], params["api"])
     project = client.get_project(params["project"])
@@ -49,5 +34,4 @@ def test_coding_recipes_complexity(params):
             assert code_analysis.loc < 2000
             assert code_analysis.lloc < 50
             v = cc_visitors.ComplexityVisitor.from_code(payload)
-            assert v.complexity < 21, "Code complexity of recipe " + recipe[
-                "name"] + " is too complex: " + v.complexity + " > max value (21)"
+            assert v.complexity < 21, "Code complexity of recipe " + recipe["name"] + " is too complex: " + v.complexity + " > max value (21)"
