6d8fcdbbcb7e175f0c9560afbf57b105f5b93d25
Christelle Ren <58270436+renchristelle@users.noreply.github.com>
2021-10-25 18:06:58 +0200

    Delete helpers.py

--- 1_project_validation/helpers.py
+++
@@ -1,8 +0,0 @@
-import dataikuapi
-
-def trigger_scenario(
-        scenario_name: dataikuapi.dss.scenario.DSSScenario
-):
-    scenario_fire = scenario_name.run() # manual trigger
-    scenario_run = scenario_fire() #
-    return scenario_run
